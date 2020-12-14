a95a7902fd863497f0e7c5db0c4aa9c9845ef745
DESKTOP-1B4SU0S\Nikita <nikkofann@gmail.com>
2020-12-15 00:39:53 +0200

    little fixed

--- src/App.js
+++ src/App.js
@@ -9,36 +9,24 @@ import ProgramPlan from "./components/ProgramPlan";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import CreditInfo from "./components/CreditInfo";
 import Test from "./components/Test";
-import { CSSTransition, TransitionGroup } from "react-transition-group";
-const createBrowserHistory = require("history").createBrowserHistory;
 
 function App() {
-  const history = createBrowserHistory();
-
   return (
-    <Router history={history}>
+    <Router>
       <div className="App">
-        <Route
-          render={({ location }) => (
-            <TransitionGroup component={null}>
-              <CSSTransition key={location.key} timeout={200} classNames="fade">
-                <Switch>
-                  <Route exact path="/">
-                    <Nav />
-                    <Intro />
-                    <Benefits />
-                    <Reviews />
-                    <ProgramPlan />
-                    <Prices />
-                  </Route>
-                  <Route path="/pay" component={CreditInfo} />
-                  <Route path="/test" component={Test} />
-                  <Route render={() => <CreditInfo />} />
-                </Switch>
-              </CSSTransition>
-            </TransitionGroup>
-          )}
-        />
+        <Switch>
+          <Route exact path="/">
+            <Nav />
+            <Intro />
+            <Benefits />
+            <Reviews />
+            <ProgramPlan />
+            <Prices />
+          </Route>
+          <Route path="/pay" component={CreditInfo} />
+          <Route path="/test" component={Test} />
+          <Route render={() => <CreditInfo />} />
+        </Switch>
       </div>
     </Router>
   );
